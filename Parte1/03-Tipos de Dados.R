d50e0a1fc327eeca1619ed3fbffa99b32be27604
brendojn <brendojnio@gmail.com>
2020-03-30 17:59:26 -0300

    Primeiro commit

---
+++ Parte1/03-Tipos de Dados.R
@@ -0,0 +1,68 @@
+
+num = 2.5
+num
+
+
+num2 = 4
+num2
+
+
+x = -123
+x
+class(x)
+
+
+is.integer(num2)
+y = as.integer(num2)
+class(y)
+
+
+x2 = 3.14
+x2
+
+
+y2 = as.integer(3.14)
+y2
+
+
+as.integer('4.27')
+as.integer("Joe")
+as.integer('Joe')
+as.integer(TRUE)
+
+
+char1 = 'A'
+char1
+
+char2 = "bolha"
+char2
+
+
+compl = 2.5 + 4i
+compl
+class(compl)
+
+
+sqrt(-1)
+sqrt(-1+0i)
+sqrt(as.complex(-1))
+        
+
+x = 1; y = 2
+z = x > 1
+z
+class(z)
+
+
+u = TRUE; v = FALSE 
+class(u)
+u & v
+u | v
+!u
+
+
+5/0
+0/5
+
+
+'Joe'/5
---
+++ Parte1/06-Vetores.R
@@ -0,0 +1,84 @@
+
+vetor_caracter = c("Olá", "DSA")
+vetor_caracter
+
+
+vetor_numerico = c(1.90, 45.3, 300.5)
+vetor_numerico
+
+
+vetor_complexo = c(5.2+3i, 3.8+4i)
+vetor_complexo
+
+
+vetor_logico = c(TRUE, FALSE, TRUE, FALSE, FALSE)
+vetor_logico
+
+
+vetor_integer = c(2, 4, 6)
+vetor_integer
+
+
+vetor1 = seq(1:10)
+vetor1
+is.vector(vetor1)
+
+
+vetor2 = rep(1:5)
+vetor2
+
+
+a <- c(1,2,3,4,5)
+a
+a[1]
+a[6]
+
+
+b <- c("Data", "Science")
+b
+b[1]
+
+
+v1 = c(2, 3, 5)
+v2 = c("aa", "bb", "cc", "dd", "ee")
+c(v1, v2)
+
+
+x = c(1, 3, 5, 7)
+y = c(2, 4, 6, 8)
+
+x * 5
+x + y
+x - y
+x * y
+x / y
+
+
+alfa = c(10, 20, 30)
+beta = c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+alfa + beta
+
+
+v = c("Barack", "Obama")
+v
+names(v) = c("Nome", "Sobrenome")
+v
+v["Nome"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
